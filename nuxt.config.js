e9d8eaf9c4866847737ff91e64b0c64b8330c125
YbCloud <abso8787@naver.com>
2018-05-15 11:31:00 +0900

    0515_3

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,11 +3,11 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'testdog',
+    title: '개사랑_yb',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: '대한민국 애견인 모임' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
