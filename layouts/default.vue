723fadb713fd44e9877e250eb611873523701ca4
YbCloud <abso8787@naver.com>
2018-05-29 12:50:38 +0900

    20180529_05

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,23 @@
 <template>
   <div>
+    <nav class = "navbar is-active">
+      <div class="navbar-brand">
+        <nuxt-link to= "/" class="navbar-item">개사랑</nuxt-link>  
+      </div>  
+      <div class="navbar-menu is-active">
+          <div class="navbar-end">
+            <nuxt-link to = "/about" class="navbar-item">소개</nuxt-link>
+          </div>
+      </div>
+    </nav>
     <nuxt/>
+    <footer class="footer has-text-centered">
+      <p></p>
+      2018년 목원대학교 개사랑, 모든 저작권은 개사랑에 있습니다.
+    </footer>
   </div>
 </template>
 
 <style>
+  
 </style>
